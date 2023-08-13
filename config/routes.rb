11ff2769bf9db00eae0a3bd8a42b0b25c7845d3a
guess01325 <guess01325@gmail.com>
2023-08-13 17:49:07 -0400

    fixed ruby routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
+  post '/auth/login', to: 'authentication#login'
+  get 'auth/verify', to: 'authentication#verify'
+  
+  resources :users, only: :create
+  # resources :students
+  # resources :volunteers
+  resources :events do
+    resources :volunteers, shallow: true 
+      resources :students, shallow: true 
+      end
+
+  resources :registrations
+
+  resources :mentorings
+  get "tags/data", to: "mentorings#default_values"
+
+
+
+    
+
+   
+  
+
+    
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
