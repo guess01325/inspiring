391746317109451854df948541fb4b2202b6ff01
guess01325 <guess01325@gmail.com>
2023-08-13 18:02:59 -0400

    edit environment production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  Rails.application.routes.default_url_options = { host: "https://inspiring-8e2fb22f97b2.herokuapp.com/" }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
