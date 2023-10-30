8e03e490a7b5d3271f59fd9f07a3c8044e8e5682
guess01325 <guess01325@gmail.com>
2023-10-30 19:09:55 -0400

    configured coors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://clinquant-cranachan-d8dc7b.netlify.app"
+    origins '*'
 
     
 
-    resource "*",
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
