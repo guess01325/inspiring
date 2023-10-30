84a69cd93f016dd6b08fb46c65895a013e2247bd
guess01325 <guess01325@gmail.com>
2023-10-30 15:22:23 -0400

    updated config production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,40 +14,39 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  # Enable server timing
-  config.server_timing = true
+  # # Enable server timing
+  # config.server_timing = true
 
-  config.serve_static_files = true # To serve static assets directly
+  # config.serve_static_files = true # To serve static assets directly
  
 
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-    config.cache_store = :null_store
-  end
 
   # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  # config.active_support.deprecation = :log
 
   # Raise exceptions for disallowed deprecations.
-  config.active_support.disallowed_deprecation = :raise
+  # config.active_support.disallowed_deprecation = :raise
 
   # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
+  # config.active_support.disallowed_deprecation_warnings = []
 
   # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
+  # config.active_record.migration_error = :page_load
 
   # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
+  # config.active_record.verbose_query_logs = true
+  config.log_level = :info
+
+
+
+  config.log_tags = [ :request_id ]
+
+  config.i18n.fallbacks = true
 
 
   # Raises error for missing translations.
@@ -56,6 +55,25 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+``  # Uncomment if you wish to allow Action Cable access from any origin.
+# config.action_cable.disable_request_forgery_protection = true
+config.active_support.deprecation = :notify
+
+config.active_support.disallowed_deprecation = :log
+
+config.active_support.disallowed_deprecation_warnings = []
+
+config.log_formatter = ::Logger::Formatter.new
+
+if ENV["RAILS_LOG_TO_STDOUT"].present?
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
+end
+
+
+config.active_record.dump_schema_after_migration = false
+
+
+
 end
